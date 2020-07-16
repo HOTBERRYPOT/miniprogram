41fda4d0c91efbff804b66a0400da9ce8483132c
unknown <unknown@example.com>
2020-07-16 15:55:23 +0800

    Initial Commit

---
+++ pages/party-getin/party-getin.js
@@ -0,0 +1,84 @@
+// pages/party-getin/party-getin.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navItems: [
+      {
+        name: '加入社团',
+        url: 'party-join',
+      },
+      {
+        name: '活动报名',
+        url: 'party-activi',
+      },
+      {
+        name: '个人信息',
+        url: 'out'
+      },
+      {
+        name: '账户信息',
+        url: 'bill'
+      },
+
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
